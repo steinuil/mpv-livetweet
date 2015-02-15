2254533d76ca39c9244afb745bf6174cf9e0c734
steinuil <steenuil.owl@gmail.com>
2015-02-15 17:07:42 +0100

    I'm a fucking dumbass. feel free to slap me with a large trout.

--- mpv-livetweet.lua
+++ mpv-livetweet.lua
@@ -1,7 +1,8 @@
 -- Options
-oauth-token = "Paste the oauth_token key here"
-oauth-token-secret = "Paste the oauth_token_secret key here"
-shot = "/tmp/shot.png"
+token = "Paste the oauth_token key here"
+token_secret = "Paste the oauth_token_secret key here"
+
+shot = "/tmp/shot.png"	-- edit this only if you're using windows
 
 -- Script
 local twitter = require 'luatwit'
@@ -10,17 +11,18 @@ local mputils = require 'mp.utils'
 local keys = {
 	consumer_key = "7svu0BZBvEqCuA3XbCUbXoHPA",
 	consumer_secret = "6U8RyN55VctvYtoh0mgBqUO8p2gLqQFvFP0MRDLhUhOEOMrZTi",
-	oauth_token = "oauth-token",
-	oauth_token_secret = "oauth-token-secret"
+	oauth_token = token,
+	oauth_token_secret = token_secret
 }
 
 function tweet()
-	mp.resume()
 	mp.commandv("screenshot_to_file", shot, "subtitles")
-	local file = io.open(shot)
-	local img_data = file:read("*a")
-	file:close()
+	local openshot = io.open(shot)
+	local img_data = openshot:read("*a")
+	openshot:close()
 	os.remove(shot)
+
+	mp.resume()
 
 	local client = twitter.new(keys)
 	local tw, headers = client:tweet_with_media{
--- print-to-stdout.applescript
+++ print-to-stdout.applescript
@@ -1,5 +1,4 @@
-tell application "System Events"
+tell application "mpv"
 	set tweet to text returned of (display dialog "" default answer "" buttons "Tweet" default button 1)
 end tell
-tell application "mpv" to activate
 do shell script "echo " & quoted form of tweet
